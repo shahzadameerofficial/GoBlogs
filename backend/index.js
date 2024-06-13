9ffbe3d972b2a91a80e1676287285168b2f36925
shahzadameerofficial <163984661+shahzadameerofficial@users.noreply.github.com>
2024-06-13 16:43:20 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 
 const corsOptions = {
     credentials: true,
-    'Access-Control-Allow-Origin': 'https://go-blogs-iq7w.vercel.app/'
+    origin: 'https://go-blogs-iq7w.vercel.app'
 }
 
 const app = express();
