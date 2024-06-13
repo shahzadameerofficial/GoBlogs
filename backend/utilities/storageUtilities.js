3433b7a4e3bd03eb60e9745d7006d573f1f902b1
shahzadameerofficial <163984661+shahzadameerofficial@users.noreply.github.com>
2024-06-13 17:21:55 +0500

    Update storageUtilities.js

--- backend/utilities/storageUtilities.js
+++ backend/utilities/storageUtilities.js
@@ -1,44 +1,40 @@
-const { BACKEND_SERVER_PATH } = require('../config/index');
-const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
 
 const storageUtilities = {
     saveImage(photo, authorId) {
-        // 1. Save Image to Storage
+        // 1. Save Image to Cloud
         // 2. Return Image Path
-
-        const buffer = Buffer.from(photo.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), 'base64');
-
-        const fileName = `${Date.now()}-${authorId}`;
+        const publicId = `${Date.now()}-${authorId}`;
+        let response;
         try {
-            fs.writeFileSync(`storage/${fileName}`, buffer) // Storing Image
+            response = cloudinary.uploader.upload(photo,{folder: 'goblogs', public_id: publicId})
         } catch (error) {
-            return error
+            console.log(error)
         }
-        const imagePath = `${BACKEND_SERVER_PATH}/storage/${fileName}`
-        return imagePath
+        let optimizeUrl = cloudinary.url(publicId, {
+            fetch_format: 'auto',
+            quality: 'auto'
+        })
+        return optimizeUrl;
+         
     },
     updateImage(previousImage, newImage, authorId) {
         // 1. Delete Previous Image from Storage
         // 2. Save New Image to Storage
         // 3. Return New Image Path
 
-        let previousPhoto = previousImage;
-        previousPhoto = previousPhoto.split('/').at(-1);
-
-        fs.unlinkSync(`storage/${previousPhoto}`); // Delete Previous Image from Storage
-
-        const buffer = Buffer.from(newImage.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), 'base64');
-
-        const fileName = `${Date.now()}-${authorId}`;
-
+        const publicId = `${Date.now()}-${authorId}`;
+        let response;
         try {
-            fs.writeFileSync(`storage/${fileName}`, buffer) // Storing Image
+            response = cloudinary.uploader.upload(newImage,{folder: 'goblogs', public_id: publicId})
         } catch (error) {
-            return error
+            console.log(error)
         }
-
-        const imagePath = `${BACKEND_SERVER_PATH}/storage/${fileName}`
-        return imagePath
+        let optimizeUrl = cloudinary.url(publicId, {
+            fetch_format: 'auto',
+            quality: 'auto'
+        })
+        return optimizeUrl;
     }
 }
 
